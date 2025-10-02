d0e914b2c9125a087f31d3c9c3ce5471da8addfa
SebaG97 <sebagiret@gmail.com>
2025-10-02 09:52:21 -0300

    checkpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.100.218:8000/api'  // ✅ IP del servidor
+  apiUrl: 'http://192.168.100.204:8000/api'  // ✅ IP del servidor
 };
